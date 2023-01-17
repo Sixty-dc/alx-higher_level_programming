aa6326b015186ddcdbb646029501bcc75123bddd
Sixty-dc <sikisi.mp@gmail.com>
2023-01-17 11:11:06 -0800

    you

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
+""" Get status in https://intranet.hbtn.io/status"""
+import urllib.request
 
-from urllib import request
-
-with request.urlopen("https://intranet.hbtn.io/status") as response:
-    r = response.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(r), r, r.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}\n\t- content: {}".format(type(html), html))
+        print("\t- utf8 content: {}".format(html.decode('UTF-8')))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Python script that fetches https://intranet.hbtn.io/status
-"""
-
+""" Show type and content using requests """
 import requests
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}\n\t- content: {}".format(type(r.text), r.text))
