f141807a03a8c34297526352f8ee5d7a6c60258e
Sixty-dc <sikisi.mp@gmail.com>
2023-02-09 19:38:43 +0000

    u

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-// A script that prints all characters of a Star Wars movie
-
 const request = require('request');
+const { argv } = require('process');
 
-const movie = process.argv[2];
-const api = 'https://swapi-api.hbtn.io/api/';
-const url = api + 'films/' + movie + '/';
-request.get({ url: url }, function (error, response, body) {
-  if (!error) {
-    const characters = (JSON.parse(body)).characters;
-    for (const character of characters) {
-      request.get({ url: character }, function (error, response, body) {
-        if (!error) {
-          process.stdout.write(((JSON.parse(body)).name) + '\n');
-        }
-      });
-    }
+let url = 'https://swapi-api.hbtn.io/api/films/';
+const movie = argv[2];
+url += movie;
+
+request(url, (error, response, body) => {
+  if (error) throw error;
+
+  const movies = JSON.parse(body).characters;
+
+  for (const characters of movies) {
+    request(characters, (error, response, body) => {
+      if (error) throw error;
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
   }
 });
